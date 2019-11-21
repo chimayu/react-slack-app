1fdf5adfa93734f439e210d7db461985a85184c8
mayumi <chimayu@gmail.com>
2019-11-22 00:21:23 +0900

    first commit

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="app">APP</div>
+    )
+  }
+}
+
+export default App;
